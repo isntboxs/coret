aac3e930ed0970458801c2724a095c06df7bf609
mrboxs <rizkywanda200155@gmail.com>
2026-06-03 04:35:02 +0700

    init

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { devtools } from '@tanstack/devtools-vite'
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+
+import tailwindcss from '@tailwindcss/vite'
+import viteReact from '@vitejs/plugin-react'
+import { nitro } from 'nitro/vite'
+import { defineConfig } from 'vite'
+
+const config = defineConfig({
+	resolve: { tsconfigPaths: true },
+	plugins: [
+		devtools(),
+		nitro({ preset: 'bun', rollupConfig: { external: [/^@sentry\//] } }),
+		tailwindcss(),
+		tanstackStart(),
+		viteReact(),
+	],
+})
+
+export default config
